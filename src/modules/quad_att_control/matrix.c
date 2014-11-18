2b43ee53644abd13829eda3cdf47577cb6dd5e0f
Brian Gasberg Thomsen <mail@briangasberg.dk>
2014-11-18 15:07:49 +0100

    added matrix/vector library

--- src/modules/quad_att_control/matrix.c
+++ src/modules/quad_att_control/matrix.c
@@ -3,13 +3,7 @@
  * Author:   Group 731 <14gr731@es.aau.dk>
  *************************************************************************/
 /**
- * Implementation of an quadrotor attitude control app for use in the 
+ * Matrix vector calculations library for use in the 
  * semester project.
  */
 
-struct quaternion_s {
-        float q1;
-        float q2;
-        float q3;
-        float q4;
-};
