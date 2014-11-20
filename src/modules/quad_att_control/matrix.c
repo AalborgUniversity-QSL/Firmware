bf0420fc62dec4acb4143aa009603097bb417578
Brian Gasberg Thomsen <mail@briangasberg.dk>
2014-11-20 10:01:48 +0100

    removed matrix libraries (own)

--- src/modules/quad_att_control/matrix.c
+++
@@ -1,9 +0,0 @@
-/*************************************************************************
- * Copyright (c) 2014 Group 731 Aalborg University. All rights reserved.
- * Author:   Group 731 <14gr731@es.aau.dk>
- *************************************************************************/
-/**
- * Matrix vector calculations library for use in the 
- * semester project.
- */
-
