2d8c4cf342f96184f0c76f7f946a90958aa431b6
Jens Nielsen <nens90@gmail.com>
2014-12-04 18:14:08 +0100

    JN

---
+++ src/modules/quad_velocity_control/module.mk
@@ -0,0 +1,7 @@
+#
+# Quadcopter velocity control app
+#
+
+MODULE_COMMAND	= quad_velocity_control
+SRCS		= quad_velocity_control_main.c
+MODULE_STACKSIZE = 1200
---
+++ src/modules/quad_velocity_control/quad_velocity_control_main.c
@@ -0,0 +1,115 @@
+/*************************************************************************
+ * Copyright (c) 2014 Group 731 Aalborg University. All rights reserved.
+ * Author:   Group 731 <14gr731@es.aau.dk>
+ *************************************************************************/
+
+/**
+ * Implementation of an quadrotor velocity control app for use in the 
+ * semester project.
+ */
+ 
+#include <nuttx/config.h>
+#include <nuttx/sched.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <math.h>
+#include <poll.h>
+#include <time.h>
+
+#include <systemlib/param/param.h>
+
+#include <mavlink/mavlink_log.h>
+
+#include <uORB/uORB.h>
+#include <uORB/topics/vehicle_status.h>
+#include <uORB/topics/quad_velocity_sp.h>
+#include <uORB/topics/quad_pos_msg.h>
+#include <uORB/topics/quad_mode.h>
+
+#include <systemlib/systemlib.h>
+#include <systemlib/err.h>
+ 
+__EXPORT int quad_velocity_control_main(int argc, char *argv[]);
+int quad_velocity_control_thread_main(int argc, char *argv[]);
+static void usage(const char *reason);
+
+
+static bool thread_running = false;
+static bool thread_should_exit = false;
+static int daemon_task;
+
+int quad_velocity_control_thread_main(int argc, char *argv[]){
+	warnx("[velocity_controller] started\n");
+
+	static int mavlink_fd;
+	mavlink_fd = open(MAVLINK_LOG_DEVICE, 0);
+
+	struct quad_pos_msg_s quad_pos;
+	memset(&quad_pos, 0, sizeof(quad_pos));
+	struct quad_mode_s quad_mode;
+	memset(&quad_mode, 0, sizeof(quad_mode));
+	struct vehicle_status_s vehicle_status;
+	memset(&vehicle_status, 0, sizeof(vehicle_status));
+
+	int quad_pos_sub = orb_subscribe(ORB_ID(quad_pos_msg));
+	int quad_mode_sub = orb_subscribe(ORB_ID(quad_mode));
+	int vehicle_status_sub = orb_subscribe(ORB_ID(vehicle_status));
+
+}
+
+
+// User interface for running in shell
+static void usage(const char *reason) {
+        if (reason)
+                fprintf(stderr, "%s\n", reason);
+
+        fprintf(stderr, "usage: velocity_controller {start|stop|status}\n\n");
+        exit(1);
+}
+
+int quad_velocity_control_main(int argc, char *argv[]) {
+        if (argc < 1)
+                usage("missing argument");
+
+        if (!strcmp(argv[1], "start")) {
+
+                if (thread_running) {
+                        printf("velocity_controller already running\n");
+
+                        exit(0);
+                }
+
+                thread_should_exit = false;
+                daemon_task = task_spawn_cmd("velocity_controller",
+                                             SCHED_DEFAULT,
+                                             SCHED_PRIORITY_MAX - 6,
+                                             2048,
+                                             quad_velocity_control_thread_main,
+                                             (argv) ? (const char **)&argv[2] : (const char **)NULL);
+                thread_running = true;
+                exit(0);
+        }
+
+        if (!strcmp(argv[1], "stop")) {
+                thread_should_exit = true;
+                exit(0);
+        }
+
+        if (!strcmp(argv[1], "status")) {
+                if (thread_running) {
+                        printf("\tvelocity_controller is running\n");
+
+                } else {
+                        printf("\tvelocity_controller not started\n");
+                }
+
+                exit(0);
+        }
+
+        usage("unrecognized command");
+        exit(1);
+}
