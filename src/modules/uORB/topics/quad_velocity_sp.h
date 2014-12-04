d5c07a5ea7aa6ff0b68001d9f67e1471daa83014
Jens Nielsen <nens90@gmail.com>
2014-12-04 17:10:52 +0100

    JN

---
+++ src/modules/uORB/topics/quad_velocity_sp.h
@@ -0,0 +1,26 @@
+/*************************************************************************
+ * Copyright (c) 2014 Group 731 Aalborg University. All rights reserved.
+ * Author:   Group 731 <14gr731@es.aau.dk>
+ *************************************************************************/
+
+/** 
+ * Topic created to handle messages from swarm control application
+ * to the quad_att_control application.
+ */
+
+#ifndef QUAD_VELOCITY_SP_H
+#define QUAD_VELOCITY_SP_H
+ 
+#include <stdint.h>
+#include "../uORB.h"
+ 
+struct quad_velocity_sp_s {
+        uint64_t timestamp;
+        float dx;    /* vector for sp */
+        float dy;
+        float z;
+};
+ 
+ORB_DECLARE(quad_velocity_sp);
+ 
+#endif
