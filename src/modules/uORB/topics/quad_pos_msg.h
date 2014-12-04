19b8ddd142ffbc2fefad53d42c9eb7cabc9a911c
Jens Nielsen <nens90@gmail.com>
2014-12-04 15:20:14 +0100

    JN

---
+++ src/modules/uORB/topics/quad_pos_msg.h
@@ -0,0 +1,30 @@
+/*************************************************************************
+ * Copyright (c) 2014 Group 731 Aalborg University. All rights reserved.
+ * Author:   Group 731 <14gr731@es.aau.dk>
+ *************************************************************************/
+
+/** 
+ * Topic created to handle position messages from ground station
+ * to the quad_formation_control application.
+ */
+
+#ifndef QUAD_POS_MSG
+#define QUAD_POS_MSG
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "../uORB.h"
+
+/* register this as object request broker structure */
+
+struct quad_pos_msg_s {
+        uint8_t target_system;   ///<  The target_system is defined in enum QUAD_FORMATION_ID
+        uint64_t timestamp;
+        float x[3];           ///< x-axis
+        float y[3];           ///< y-axis
+        float z[3];           ///< z-axis -1 if coordinate is Not Available
+};
+
+ORB_DECLARE(quad_pos_msg);
+
+#endif
---
+++ src/modules/uORB/topics/quad_swarm_cmd.h
@@ -0,0 +1,34 @@
+/*************************************************************************
+ * Copyright (c) 2014 Group 731 Aalborg University. All rights reserved.
+ * Author:   Group 731 <14gr731@es.aau.dk>
+ *************************************************************************/
+
+/*
+ Topic created to handle command messages from ground station.
+ */
+
+#ifndef QUAD_SWARM_CMD
+#define QUAD_SWARM_CMD
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "../uORB.h"
+
+/* register this as object request broker structure */
+
+enum QUAD_MSG_CMD {
+        QUAD_MSG_CMD_START_SWARM = 42,
+	QUAD_MSG_CMD_STOP_SWARM = 43,
+	QUAD_MSG_CMD_TAKEOFF = 44,
+	QUAD_MSG_CMD_LAND = 45,
+	QUAD_MSG_CMD_ENUM_END = 46
+};
+
+struct quad_swarm_cmd_s {
+	uint8_t target_system;
+	uint8_t cmd_id;
+};
+
+ORB_DECLARE(quad_swarm_cmd);
+
+#endif
