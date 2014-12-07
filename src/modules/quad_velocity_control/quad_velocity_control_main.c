a926f1c56a8e0c1cfd680c332dec88050751b392
Jens Nielsen <nens90@gmail.com>
2014-12-07 15:39:12 +0100

    JN

--- src/modules/quad_velocity_control/quad_velocity_control_main.c
+++ src/modules/quad_velocity_control/quad_velocity_control_main.c
@@ -219,7 +219,7 @@ int quad_velocity_control_thread_main(int argc, char *argv[]){
 				} else {
 					
 					shutdown_motors = true;
-					state_transition = false;
+					state_transition.land = false;
 					quad_mode.cmd = (enum QUAD_CMD)QUAD_CMD_PENDING;
 					quad_mode.current_state = (enum QUAD_STATE)QUAD_STATE_GROUNDED;
                         		orb_publish(ORB_ID(quad_mode), quad_mode_pub, &quad_mode);
