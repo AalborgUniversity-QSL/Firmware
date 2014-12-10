7615488fa4cf41cf32836c1a2b4a56fc739148dd
Jens Nielsen <nens90@gmail.com>
2014-12-10 12:55:22 +0100

    JN

--- src/modules/quad_velocity_control/quad_velocity_control_main.c
+++ src/modules/quad_velocity_control/quad_velocity_control_main.c
@@ -114,8 +114,7 @@ int quad_velocity_control_thread_main(int argc, char *argv[]){
 	     shutdown_motors = true,
 	     quad_mode_updated = false,
 	     vehicle_status_updated = false,
-	     test = false,
-	     first = true;
+	     test = false;
 
 	struct pollfd fds[1];
 	fds[0].fd = quad_pos_sub;
@@ -264,11 +263,7 @@ int quad_velocity_control_thread_main(int argc, char *argv[]){
 					// state_transition = true;
 
 				} else if (quad_mode.cmd == (enum QUAD_CMD)QUAD_CMD_PENDING){
-					// Do nothing yet
-					if ( first ) {
-						first = false;
-						mavlink_log_info(mavlink_fd,"[POT] PENDING");
-					}
+						// mavlink_log_info(mavlink_fd,"[POT] PENDING");
 				}
 
 			} else {
@@ -277,7 +272,6 @@ int quad_velocity_control_thread_main(int argc, char *argv[]){
 				initialised = false;
 
 				if ( quad_mode.current_state != (enum QUAD_STATE)QUAD_STATE_GROUNDED ) {
-					first = true;
 					quad_mode.cmd = (enum QUAD_CMD)QUAD_CMD_PENDING;
 					quad_mode.current_state = (enum QUAD_STATE)QUAD_STATE_GROUNDED;
 
