b0ab93a193db4c9051b4b8312611064725f33ca8
Brian Gasberg Thomsen <mail@briangasberg.dk>
2014-12-14 14:53:21 +0100

    bgt

--- src/modules/quad_velocity_control/swarm_formation.c
+++ src/modules/quad_velocity_control/swarm_formation.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include "swarm_formation.h"
 
-
 velocity_t wall(float posx, float posy) {
 	float ax;
 	float ay;
@@ -31,26 +30,26 @@ velocity_t wall(float posx, float posy) {
         for(int o = 0; o <= 5; ++o) {
                 ax = posx - wall1x[o];
                 ay = posy - wall1y[o];
-                bx = wall2x[o]- wall1x[o];
-                by = wall2y[o]- wall1y[o];
-                lb = sqrt( (wall2x[o] - wall1x[o]) * (wall2x[o] - wall1x[o]) + (wall2y[o] - wall1y[o]) * (wall2y[o] - wall1y[o]) );
-                dotab = ax *bx + ay * by;
-                projx = dotab / (lb * lb) * bx;
-                projy = dotab / (lb * lb) * by;
+                bx = wall2x[o] - wall1x[o];
+                by = wall2y[o] - wall1y[o];
+                lb = sqrt( ((wall2x[o] - wall1x[o]) * (wall2x[o] - wall1x[o])) + ((wall2y[o] - wall1y[o]) * (wall2y[o] - wall1y[o])) );
+                dotab = (ax * bx) + (ay * by);
+                projx = dotab / ((lb * lb) * bx);
+                projy = dotab / ((lb * lb) * by);
                 obsx = projx + wall1x[o];
                 obsy = projy + wall1y[o];
 
-                if ( (obsx < wall1x[o]) || (obsx == wall1x[o] && obsy < wall1y[o]) ) {
+                if ( (obsx < wall1x[o]) || ( (obsx == wall1x[o]) && (obsy < wall1y[o]) ) ) {
                         obsx = wall1x[o];
                         obsy = wall1y[o];
                 }
 
-                if( (obsx > wall2x[o]) || (obsx == wall2x[o] && obsy > wall2y[o]) ) {
+                if( (obsx > wall2x[o]) || ( (obsx == wall2x[o]) && (obsy > wall2y[o]) ) ) {
                         obsx = wall2x[o];
                         obsy = wall2y[o];
                 }
         
-                l = sqrt( (obsx - posx) * (obsx - posx) + (obsy - posy) * (obsy - posy) );
+                l = sqrt( ( (obsx - posx) * (obsx - posx) ) + ( (obsy - posy) * (obsy - posy) ) );
                 vm[0] = wallfield * (posx - obsx) / l * (karmdist - l) * (float)fabs(karmdist - l);
                 vm[1] = wallfield * (posy - obsy) / l * (karmdist - l) * (float)fabs(karmdist - l);
 
@@ -81,20 +80,20 @@ velocity_t swarm( int i, float allposx [10], float allposy [10] ) {
 	vel.v1 = 0;
 	vel.v2 = 0;
 
-	for(int n = 0; n < number_of_quads; ++n) {
+	for (int n = 0; n < number_of_quads; ++n) {
                 if( n != i ) {
                         allpos[0] = allposx[n];
                         allpos[1] = allposy[n];
-                        l = sqrt( (allpos[0] - pos[0]) * (allpos[0] - pos[0]) + (allpos[1] - pos[1]) * (allpos[1] - pos[1]) );
+                        l = sqrt( ( (allpos[0] - pos[0]) * (allpos[0] - pos[0]) ) + ( (allpos[1] - pos[1]) * (allpos[1] - pos[1]) ) );
                         //printf("l er  %f \n", l);
-                        vm[0] = vm[0]+potfield*allposx[n]/l*(l-dist);
-                        vm[1] = vm[1]+potfield*allposy[n]/l*(l-dist);;
+                        vm[0] = vm[0] + ( (potfield * allposx[n]) / ( l * (l - dist) ) );
+                        vm[1] = vm[1] + ( (potfield * allposy[n]) / ( l * (l - dist) ) );
 			//printf("for %i v1 = %f, v2 = %f \n", n, vm[0],vm[1]);
                         // To make them not chrash into each other
-                        lvm = sqrt(vm[0]*vm[0]+vm[1]*vm[1]);
-                        if(l > dist && lvm > vmax*drag) {
-                                vm[0]= vmax*drag*vm[0]/lvm;
-                                vm[1]= vmax*drag*vm[1]/lvm;
+                        lvm = sqrt( (vm[0] * vm[0]) + (vm[1] * vm[1]) );
+                        if ( (l > dist) && ( lvm > (vmax * drag) ) ) {
+                                vm[0] = (vmax * drag * vm[0]) / lvm;
+                                vm[1] = (vmax * drag * vm[1]) / lvm;
                                 //printf("for %i v1 = %f, v2 = %f \n", n, vm[0],vm[1]);
                         }
                         vel.v1 = vel.v1 + vm[0];
